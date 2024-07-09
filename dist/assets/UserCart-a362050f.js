31cc447ab78f7e138ff2ef1234c5041682965452
yogesh <shelkeyogesh874@gmail.com>
2024-07-09 09:40:26 +0530

    initial setup

---
+++ dist/assets/UserCart-a362050f.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-44156e21.js";import{N as s,F as t}from"./Footer-2b3ac1f8.js";import{C as o}from"./Cart-d27e9eac.js";import"./index-dbeac661.js";const i=()=>r.jsxs(r.Fragment,{children:[r.jsx(s,{}),r.jsx(o,{}),r.jsx(t,{})]});export{i as default};
